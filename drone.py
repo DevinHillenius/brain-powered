d7b301d45dc8a0998f19648e91b1468133c3a6c9
Devin Hillenius <devinhillenius@quicknet.nl>
2018-04-05 12:46:36 +0200

    Update drone wrap class to work with new library

--- drone.py
+++ drone.py
@@ -3,13 +3,14 @@
 # By Derk Barten and Devin Hillenius
 # UvA Brain Powered 2017-2018
 
-import ardrone
+from pyardrone import ARDrone
 from time import sleep
 
 
 class Drone(object):
     def __init__(self):
-        self.d = ardrone.ARDrone()
+        self.d = ARDrone()
+        self.d.navdata_ready.wait()
 
     def takeoff(self):
         """Start flying the drone."""
@@ -19,7 +20,7 @@ class Drone(object):
         """Land the drone."""
         self.d.land()
 
-    def move(self, direction, t=1):
+    def move(self, direction, t=1, speed=1):
         """
         Move the drone forward or backward or rotate the drone for 't' seconds.
         The 'direction' argument can be:
@@ -29,20 +30,20 @@ class Drone(object):
             - 'rotate_left': rotate 90 degrees to the left
         """
         if direction == 'forward':
-            self.d.move_forward()
+            self.d.move(forward=speed)
         elif direction == 'backward':
-            self.d.move_backward()
+            self.d.move(backward=speed)
         elif direction == 'rotate_right':
-            self.d.turn_right()
+            self.d.move(cw=speed)
         elif direction == 'rotate_left':
-            self.d.turn_left()
+            self.d.move(ccw=speed)
         else:
             raise ValueError(
                 'Given direction {} not supported!'.format(direction))
         sleep(t)
         self.d.hover()
 
-    def print_battery():
-        """Print the battery percentage of the drone to stdout."""
-        print("Drone battery percentage: {}".format(drone.navdata['demo'][
-            'battery']))
+    # def print_battery():
+    #     """Print the battery percentage of the drone to stdout."""
+    #     print("Drone battery percentage: {}".format(drone.navdata['demo'][
+    #         'battery']))
